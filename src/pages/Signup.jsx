58a0df32d2996237f80e35cd9bd3c64eed20df40
Harry EMARH <emarh.harry.code@gmail.com>
2025-05-01 23:16:16 +0200

    Sign Page 2

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  return (
+    <section className="h-screen flex items-center justify-center relative overflow-hidden bg-[url('../../assets/images/hero/bg3.jpg')] bg-no-repeat bg-center bg-cover">
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-slate-900"></div>
+      <div className="container">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+          <div className="relative overflow-hidden bg-white dark:bg-slate-900 shadow-md dark:shadow-gray-800 rounded-md">
+            <div className="p-6">
+              <Link to="/">
+                <img src="/assets/images/logo-dark.png" className="mx-auto h-[24px] block dark:hidden" alt="logo dark" />
+                <img src="/assets/images/logo-light.png" className="mx-auto h-[24px] dark:block hidden" alt="logo light" />
+              </Link>
+              <h5 className="my-6 text-xl font-semibold">Signup</h5>
+              <form className="text-start">
+                <div className="grid grid-cols-1">
+                  <div className="mb-4 text-start">
+                    <label className="font-semibold" htmlFor="RegisterName">Your Name:</label>
+                    <input id="RegisterName" type="text" className="form-input mt-3 rounded-md" placeholder="Harry" />
+                  </div>
+
+                  <div className="mb-4 text-start">
+                    <label className="font-semibold" htmlFor="LoginEmail">Email Address:</label>
+                    <input id="LoginEmail" type="email" className="form-input mt-3 rounded-md" placeholder="name@example.com" />
+                  </div>
+
+                  <div className="mb-4 text-start">
+                    <label className="font-semibold" htmlFor="LoginPassword">Password:</label>
+                    <input id="LoginPassword" type="password" className="form-input mt-3 rounded-md" placeholder="Password:" />
+                  </div>
+
+                  <div className="mb-4">
+                    <div className="flex items-center mb-0">
+                      <input className="form-checkbox size-4 border border-gray-200 dark:border-gray-800 accent-green-600 me-2" type="checkbox" id="AcceptTAC" />
+                      <label htmlFor="AcceptTAC" className="text-slate-400">I Accept <a href="#" className="text-emerald-600">Terms And Condition</a></label>
+                    </div>
+                  </div>
+
+                  <div className="mb-4">
+                    <input type="submit" className="py-1 px-5 w-full font-semibold bg-emerald-600 hover:bg-emerald-700 border border-emerald-600 hover:border-emerald-700 text-white rounded-md" value="Register" />
+                  </div>
+
+                  <div className="text-center">
+                    <span className="text-slate-400 me-2">Already have an account?</span>
+                    <Link to="/" className="text-slate-900 dark:text-white font-bold">Sign in</Link>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            <div className="px-6 py-2 bg-slate-50 dark:bg-slate-800 text-center">
+              <p className="mb-0 text-gray-400 font-medium">
+                © {new Date().getFullYear()} Jobstack. Designed by <a href="https://shreethemes.in/" target="_blank" className="text-reset">Shreethemes</a>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
