fb3478cc85e6da03af0155474801cb35b931ecf7
Harry EMARH <emarh.harry.code@gmail.com>
2025-05-03 21:22:33 +0200

    Update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import './App.css';
 
 function App() {
   return (
