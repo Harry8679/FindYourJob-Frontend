b6703d0d3df0c4d8725ebc5fbf377ce1568e239c
Harry EMARH <emarh.harry.code@gmail.com>
2025-05-04 12:02:21 +0200

    Update 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,36 @@ const Login = () => {
                 <img src={LogoLight} className="mx-auto h-[24px] dark:block hidden" alt="logo light" />
               </Link>
               <h5 className="my-6 text-xl font-semibold">Login</h5>
-                Test
+              <form className="text-start">
+                <div className="grid grid-cols-1">
+                  <div className="mb-4 text-start">
+                    <label className="font-semibold" htmlFor="LoginEmail">Email Address:</label>
+                    <input id="LoginEmail" type="email" className="form-input mt-3 rounded-md" placeholder="name@example.com" />
+                  </div>
+
+                  <div className="mb-4 text-start">
+                    <label className="font-semibold" htmlFor="LoginPassword">Password:</label>
+                    <input id="LoginPassword" type="password" className="form-input mt-3 rounded-md" placeholder="Password:" />
+                  </div>
+
+                  <div className="flex justify-between mb-4">
+                    <div className="inline-flex items-center mb-0">
+                      <input className="form-checkbox size-4 border border-gray-200 dark:border-gray-800 accent-green-600 me-2" type="checkbox" id="RememberMe" />
+                      <label htmlFor="RememberMe" className="text-slate-400">Remember me</label>
+                    </div>
+                    <p className="text-slate-400 mb-0"><Link to="/reset-password" className="text-slate-400">Forgot password?</Link></p>
+                  </div>
+
+                  <div className="mb-4">
+                    <input type="submit" className="py-1 px-5 w-full font-semibold bg-emerald-600 hover:bg-emerald-700 border border-emerald-600 hover:border-emerald-700 text-white rounded-md" value="Login / Sign in" />
+                  </div>
+
+                  <div className="text-center">
+                    <span className="text-slate-400 me-2">Don't have an account?</span>
+                    <Link to="/inscription" className="text-slate-900 dark:text-white font-bold">Sign Up</Link>
+                  </div>
+                </div>
+              </form>
             </div>
 
             <div className="px-6 py-2 bg-slate-50 dark:bg-slate-800 text-center">
